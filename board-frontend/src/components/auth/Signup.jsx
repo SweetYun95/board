fafb7bc5d1da7c308aa172bef1608b9de5a55eb3
SweetYun95 <isr01154@naver.com>
2025-07-15 18:16:59 +0900

    2507151816

--- board-frontend/src/components/auth/Signup.jsx
+++ board-frontend/src/components/auth/Signup.jsx
@@ -1,5 +1,50 @@
+import { TextField, Button, Container, Typography, CircularProgress } from '@mui/material'
+
+import { useEffect, useState } from 'react'
+import { useDispathch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import { joinUserThunk, clearAuthError } from '../../features/authSlice'
+
 function Signup() {
-    return (  );
+   const [email, setEmail] = useState('')
+   const [name, setName] = useState('')
+   const [password, setPassword] = useState('')
+
+   const dispatch = useDispathch()
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        return () => {
+            dispatch(clearAuthError())
+        }
+    },[dispatch])
+
+   return (
+      <Container maxWidth="sm">
+         <Typography variant="h4" gutterBottom>
+            회원가입
+         </Typography>
+
+         {error && (
+            <Typography color="error" align="center">
+               {error}
+            </Typography>
+         )}
+
+         <TextField label="이메일" variant="outlined" fullWidth margin="normal" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+         <TextField label="사용자 이름" variant="outlined" fullWidth margin="normal" value={nick} onChange={(e) => setNick(e.target.value)} />
+
+         <TextField label="비밀번호" variant="outlined" type="password" fullWidth margin="normal" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+         <TextField label="비밀번호 확인" variant="outlined" type="password" fullWidth margin="normal" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+
+         <Button variant="contained" color="primary" onClick={handleSignup} fullWidth disabled={loading} style={{ marginTop: '20px' }}>
+            {loading ? <CircularProgress size={24} /> : '회원가입'}
+         </Button>
+      </Container>
+   )
 }
 
-export default Signup;
+export default Signup
